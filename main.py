32bbcbfeabd1f8dc61bfe5f0878bd4d94f8d4a74
Mohammad harb <136888573+MohammadHb82@users.noreply.github.com>
2023-07-08 01:17:05 -0700

    added home come work

---
+++ main.py
@@ -0,0 +1,98 @@
+
+#homework python_SDK:
+
+#Storing data in variables:
+
+#easy:
+
+#Store your name in a variable and then output it on the screen#
+"""input_name=input("enter your name")
+print(input_name)
+"""
+#medium task:
+
+#Store the current month in a variable and then output on the screen along with the message: “The current month is......”
+"""
+current_var="julay"
+
+print("The current month is",current_var)
+"""
+#hard task:
+
+#the screen save your name into one variable and your age into another. Output the variables onto the screen in the following way: “My name is Carl, and I am 16 years old”
+"""
+name="carl"
+age=16
+print("my name is",name ,"and my age is",age ,"years old")
+
+"""
+#Inputting Data
+
+#easy:
+
+#Ask the user for their favorite animal and save this in a (with a suitable name)
+
+"""
+asking_user=input("what's your favourite animal sir")
+answer_user=asking_user
+"""
+#Medium task:
+
+#Write a program that does the following:
+#●What is your first name? John
+#●What is your last name? Smith
+#●Hello John Smith 
+"""
+firstname=(input("what is your first name"))
+print("my first name is",firstname)
+"""
+#hard_task:
+
+#Accept a country from the keyboard and then its population. 
+#On the screen output the results in the following format:
+#Brazil has a population of 32000000 people.
+"""
+Cn_name=input("write your country name")
+population=int(input("write your population"))
+print(Cn_name,"has a population of",population,"poeple")
+"""
+#Calculations:
+
+#Easy task:
+
+#Create a program that accepts two numbers from the keyboard then shows the result of adding them onto the screen.
+"""
+calc_num1=int(input("enter  number one"))
+calc_num2=int(input("enter number two"))
+Calculations=calc_num1 + calc_num2
+print(Calculations)
+"""
+#Medium task:
+
+#Write a program that does the following:
+#●What is your first number? 12
+#●What is your second number? 120
+#●120 divided 12 equals 10
+"""
+num1=int(input("what's your first number"))
+num2=int(input("what's your secound number"))
+if num2/num1==10:
+    print("correct  answer")
+else:
+    print("uncorrect answer")
+"""
+#hard task:
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
